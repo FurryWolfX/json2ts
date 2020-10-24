3551261523c0fe1c2062d83a56c1c953c3afb736
wolfx <digimonx@qq.com>
2020-10-24 15:22:43 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <a-row>
+      <a-col :span="12">
+        <a-textarea v-model="input" placeholder="请输入JSON" :auto-size="{ minRows: 30, maxRows: 30 }" />
+      </a-col>
+      <a-col :span="12">
+        <a-textarea :value="output" placeholder="输出的interface" :auto-size="{ minRows: 30, maxRows: 30 }" readonly />
+      </a-col>
+    </a-row>
+    <a-divider>参数</a-divider>
+    <a-row>
+      <a-col :span="24" style="text-align: center">
+        <a-select default-value="JSON" style="width: 300px" size="large" @change="handleCodeTypeChange">
+          <a-select-option value="JSON"> JSON </a-select-option>
+          <a-select-option value="JAVA"> Java Class </a-select-option>
+        </a-select>
+        <a-input v-model="name" style="width: 300px" placeholder="请输入接口名称" size="large"> </a-input>
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import typeofJson from "typeof-jsonc/dist";
+
+type CodeType = "JAVA" | "JSON";
+
+@Component
+export default class App extends Vue {
+  private codeType: CodeType = "JSON";
+  private name: string = "Result";
+  private input: string = `{"name": "wolfx.cn"}`;
+
+  get output(): string {
+    if (this.codeType === "JAVA") {
+      const ast = this.parseJavaEntity(this.input);
+      const props = ast.result.join("\n");
+      let result = "";
+      result += `export interface ${ast.className} {\n`;
+      result += props;
+      result += "\n}";
+      return result;
+    } else {
+      try {
+        return typeofJson(this.input, this.name, {
+          prefix: "",
+          rootFlags: 1,
+          disallowComments: false,
+          addExport: true,
+          singleLineJsDocComments: true,
+        });
+      } catch (e: Error) {
+        return e.stack;
+      }
+    }
+  }
+
+  handleCodeTypeChange(type: CodeType) {
+    this.codeType = type;
+  }
+
+  parseJavaEntity(code: string) {
+    const reg = /private (\w+) (\w+);/g;
+    const resList: string[] = code.match(reg);
+    const result: string[] = [];
+    resList.forEach((r) => {
+      const res = reg.exec(r);
+      if (res && res.length) {
+        result.push(`${res[2]}: ${this.parseJavaType(res[1])}`);
+      }
+    });
+
+    const regClassName = /class (\w+)/;
+    const resClassName = regClassName.exec(code);
+    let className = "";
+    if (resClassName && resClassName.length) {
+      className = resClassName[1];
+    }
+    return {
+      result,
+      className,
+    };
+  }
+
+  parseJavaType(type: string) {
+    switch (type) {
+      case "String":
+        return "string;";
+      case "Date":
+        return "string; // Date";
+      case "Integer":
+        return "number;";
+      case "BigDecimal":
+        return "number;";
+      default:
+        return type + ";";
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  text-align: center;
+  img {
+    height: 200px;
+  }
+}
+.title {
+  text-align: center;
+}
+</style>
